cb8cc648a53c7b0a4f135d71170ea2728ac5637c
Dima Granetchi <system.grand@gmail.com>
2021-04-15 00:11:56 +0300

    correct goto import:t path

--- server/src/server.ts
+++ server/src/server.ts
@@ -654,6 +654,13 @@ function onDefinition(uri: string, blkFile: BlkBlock, position: Position, onlyEx
 	const param = getParamAt(blkFile, position)
 	if (!param)
 		return { res: [] }
+	if (param.depth == 0 && (param.res?.value?.length ?? 0) >= 3 && param.res?.value[0] == "import" && param.res?.value[1] == "t") {
+		const inc = removeQuotes(param.res.value[2])
+		return {
+			include: inc,
+			res: [{ name: inc, filePath: findWSFile(inc, dirname(URI.parse(uri).fsPath)), location: BlkLocation.create(), prefix: false }],
+		}
+	}
 	if ((param.res?.value?.length ?? 0) >= 3
 		&& (!onlyExtends || (param.depth == 1 && param.res.value[0] == extendsField && param.res.value[1] == "t"))) {
 		const startOffset = (param.res.value[0].length + param.res.value[1].length + param.res.value[2].length) - (param.res.location.end.column - 1 - position.character)
@@ -692,13 +699,6 @@ function onDefinition(uri: string, blkFile: BlkBlock, position: Position, onlyEx
 			include: param.include.value,
 			res: [{ name: param.include.value, filePath: findWSFile(param.include.value, dirname(URI.parse(uri).fsPath)), location: BlkLocation.create(), prefix: false }],
 		}
-	if (param.depth == 0 && (param.res?.value?.length ?? 0) >= 3 && param.res?.value[0] == "import") {
-		const inc = removeQuotes(param.res.value[2])
-		return {
-			include: inc,
-			res: [{ name: inc, filePath: findWSFile(inc, dirname(URI.parse(uri).fsPath)), location: BlkLocation.create(), prefix: false }],
-		}
-	}
 	return { res: [] }
 }
 
